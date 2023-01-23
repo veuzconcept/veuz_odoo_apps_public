d75404c19c24c616c5ce91fe34a80308ebde1a6c
user <albin.john@veuz.in>
2023-01-23 15:53:39 +0530

    ddel mod

--- vz_bank_charges/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import vz_account_journal
-from . import vz_register_payment
-from . import vz_account_payment
-from . import vz_res_config
-
-
-
--- vz_bank_charges/models/vz_account_journal.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class AccountJournalInherit(models.Model):
-    _inherit = "account.journal"
-
-    bank_charge_account = fields.Many2one('account.account')
